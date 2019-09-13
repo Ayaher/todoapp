4b1ce9ff2ea61686a60d00b1bc0672e4144bfe5b
Afonso Filho <afonso@acception.com.br>
2019-09-13 14:59:36 -0300

    Pequenos ajustes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM node:12 AS builder
 
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+RUN apt-get update && apt-get install -y google-chrome-stable
+
+ENV CHROME_BIN=chromium
+
 WORKDIR /app
 
 COPY index.html .
@@ -9,8 +17,8 @@ COPY package.json .
 
 RUN npm install
 #RUN npm install karma-jasmine karma-chrome-launcher --save-dev
+#RUN rm -rf node_modules/ts-node
 #RUN npm test
-RUN ls -l
 
 FROM httpd:2.4-alpine
 RUN rm -rf /usr/local/apache2/htdocs/*
--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -3,7 +3,23 @@ module.exports = function (config) {
 
 	config.set({
 		basePath: '../../',
-		frameworks: ['jasmine'],
+		frameworks: ['jasmine', '@angular/cli'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('@angular/cli/plugins/karma')
+		],
+		customLaunchers: {
+			ChromeHeadlessCI: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+		},
+		client:{
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
 		files: [
 			'node_modules/angular/angular.js',
 			'node_modules/angular-route/angular-route.js',
@@ -12,8 +28,29 @@ module.exports = function (config) {
 			'js/**/*.js',
 			'test/unit/**/*.js'
 		],
+		preprocessors: {
+			'./src/test.ts': ['@angular/cli']
+		},
+		mime: {
+			'text/x-typescript': ['ts','tsx']
+		},
+		coverageIstanbulReporter: {
+			reports: [ 'html', 'lcovonly' ],
+			fixWebpackSourcePaths: true
+		},
+		angularCli: {
+			environment: 'dev'
+		},
+		reporters: config.angularCli && config.angularCli.codeCoverage
+				? ['progress', 'coverage-istanbul']
+				: ['progress', 'kjhtml'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
 		autoWatch: true,
 		singleRun: false,
-		browsers: ['Chrome', 'Firefox']
+		browsers: ['ChromeHeadlessCI'],
+		singleRun: false,
+		browserNoActivityTimeout: 60000
 	});
 };
